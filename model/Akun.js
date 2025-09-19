d24eece13a5362c4604cb36917ff6d26de387063
panoramaqo <diandrazahwan7@gmail.com>
2025-09-19 18:00:28 +0700

    Update Akun.js

--- model/Akun.js
+++ model/Akun.js
@@ -5,7 +5,7 @@ const { API_ID, API_HASH } = require('../config/setting')
 class Akun {
   constructor(uid) {
     this.uid = uid; this.client = null; this.sess = ''; this.name = ''; this.authed = false
-    this.msgs = []; this.targets = new Map(); this.all = true; this.delay = 5; this.startAfter = 0; this.stopAfter = 0
+    this.msgs = []; this.targets = new Map(); this.all = false; this.delay = 5; this.startAfter = 0; this.stopAfter = 0
     this.running = false; this.timer = null; this.idx = 0; this.msgIdx = 0; // DITAMBAHKAN
     this.stats = {sent:0,failed:0,skip:0,start:0}
     this.pendingCode = null; this.pendingPass = null; this.pendingMsgId = null
@@ -97,13 +97,7 @@ class Akun {
         return
       }
       try {
-        let list = []
-        if (this.all) {
-          const dialogs = await this.client.getDialogs()
-          list = dialogs.filter(d => d.isGroup || d.isChannel)
-        } else {
-          list = Array.from(this.targets.values())
-        }
+        let list = Array.from(this.targets.values())
         if (!list.length || !this.msgs.length) { this.stats.skip++; return }
         if (this.idx >= list.length) this.idx = 0
         if (this.msgIdx >= this.msgs.length) this.msgIdx = 0 // DITAMBAHKAN
@@ -159,3 +153,4 @@ class Akun {
 }
 
 module.exports = Akun
+
