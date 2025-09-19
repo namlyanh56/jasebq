9f5db112667a435ca6c10d7ee68edb0942a57c5a
panoramaqo <diandrazahwan7@gmail.com>
2025-09-19 17:58:54 +0700

    Update Akun.js

--- model/Akun.js
+++ model/Akun.js
@@ -6,7 +6,8 @@ class Akun {
   constructor(uid) {
     this.uid = uid; this.client = null; this.sess = ''; this.name = ''; this.authed = false
     this.msgs = []; this.targets = new Map(); this.all = true; this.delay = 5; this.startAfter = 0; this.stopAfter = 0
-    this.running = false; this.timer = null; this.idx = 0; this.stats = {sent:0,failed:0,skip:0,start:0}
+    this.running = false; this.timer = null; this.idx = 0; this.msgIdx = 0; // DITAMBAHKAN
+    this.stats = {sent:0,failed:0,skip:0,start:0}
     this.pendingCode = null; this.pendingPass = null; this.pendingMsgId = null
   }
 
@@ -105,8 +106,9 @@ class Akun {
         }
         if (!list.length || !this.msgs.length) { this.stats.skip++; return }
         if (this.idx >= list.length) this.idx = 0
+        if (this.msgIdx >= this.msgs.length) this.msgIdx = 0 // DITAMBAHKAN
         const target = list[this.idx++]
-        const msg = this.msgs[Math.floor(Math.random() * this.msgs.length)]
+        const msg = this.msgs[this.msgIdx++] // DIUBAH
         await this.client.sendMessage(target.id || target, {message: msg})
         this.stats.sent++
       } catch(e) {
