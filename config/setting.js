0bb3d339c863bdd5d3b04234c93d260cf1b02c33
namlyanh56 <namlyanh56@gmail.com>
2025-09-22 22:52:00 +0700

    Update setting.js

--- config/setting.js
+++ config/setting.js
@@ -14,9 +14,19 @@ if (!process.env.BOT_TOKEN || !process.env.API_ID || !process.env.API_HASH) {
 const sessionsDir = path.join(__dirname, '../sessions')
 if (!fs.existsSync(sessionsDir)) fs.mkdirSync(sessionsDir, { recursive: true })
 
+const BOT_ID = parseInt(process.env.BOT_TOKEN.split(':')[0], 10);
+
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  API_ID: parseInt(process.env.API_ID),
+  API_HASH: process.env.API_HASH,
+  sessionsDir,
+  BOT_ID 8262515261
+
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   API_ID: parseInt(process.env.API_ID),
   API_HASH: process.env.API_HASH,
   sessionsDir
+
 }
